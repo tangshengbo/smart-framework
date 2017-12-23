20c42610c283eae00bf3033aaa8319c7fb119fab
tangshengbo <867850520tangbo@sian.com>
2017-12-23 22:45:16 +0800

    Initial commit

---
+++ src/main/java/com/tangshengbo/helper/ConfigHelper.java
@@ -0,0 +1,42 @@
+package com.tangshengbo.helper;
+
+import com.tangshengbo.constants.ConfigConstant;
+import com.tangshengbo.util.PropsUtil;
+
+import java.util.Properties;
+
+/**
+ * Created by TangShengBo on 2017/12/23.
+ */
+public class ConfigHelper {
+
+    private final static Properties CONFIG_PROPS = PropsUtil.loadProps(ConfigConstant.CONFIG_FILE);
+
+    public static String getJdbcDriver() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_DRIVER);
+    }
+
+    public static String getJdbcUrl() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_URL);
+    }
+
+    public static String getJdbcUsername() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_USERNAME);
+    }
+
+    public static String getJdbcPassword() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_PASSWORD);
+    }
+
+    public static String getAppBasePackage() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_BASE_PACKAGE);
+    }
+
+    public static String getAppJspPath() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_JSP_PATH, "/WEB-INF/view/");
+    }
+
+    public static String getAppAssetPath() {
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_ASSET_PATH, "/asset/");
+    }
+}
---
+++ src/main/java/com/tangshengbo/util/ClassUitl.java
@@ -0,0 +1,146 @@
+package com.tangshengbo.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * Created by TangShengBo on 2017/12/23.
+ */
+public class ClassUitl {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClassUitl.class);
+
+    /**
+     * 获取类加载器
+     *
+     * @return
+     */
+    public static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * 加载类
+     *
+     * @param className
+     * @param isInitialized
+     * @return
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        Class<?> cls;
+        try {
+            cls = Class.forName(className, isInitialized, getClassLoader());
+        } catch (ClassNotFoundException e) {
+            logger.error("load class failure {}", e);
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
+
+    /**
+     * 加载类 懒加载
+     *
+     * @param className
+     * @return
+     */
+    public static Class<?> loadClass(String className) {
+        return loadClass(className, false);
+    }
+
+    /**
+     * 获取指定包名下的所有类
+     *
+     * @param packageName
+     * @return
+     */
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (Objects.isNull(url)) {
+                    return classSet;
+                }
+                String protocol = url.getProtocol();
+                if ("file".equals(protocol)) {
+                    parseFile(packageName, classSet, url);
+                } else if ("jar".equals(protocol)) {
+                    parseJar(classSet, url);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("get class set failure {}", e);
+        }
+        return classSet;
+    }
+
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        File[] files = new File(packagePath).listFiles(file ->
+                (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory());
+        List<File> fileList =  Arrays.asList(files);
+        fileList.stream().forEach(file -> {
+            String fileName = file.getName();
+            if (file.isFile()) {
+                String className = fileName.substring(0, fileName.lastIndexOf("."));
+                if (StringUtils.isNotEmpty(packageName)) {
+                    className = packageName + "." + className;
+                }
+                doAddClass(classSet, className);
+                return;
+            }
+            String subPackagePath = fileName;
+            if (StringUtils.isNotEmpty(packagePath)) {
+                subPackagePath = packagePath + "/" + subPackagePath;
+            }
+            String subPackageName = fileName;
+            if (StringUtils.isNotEmpty(packageName)) {
+                subPackageName = packagePath + "." + subPackageName;
+            }
+            addClass(classSet, subPackagePath, subPackageName);
+        });
+
+
+    }
+
+    private static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> cls = loadClass(className);
+        classSet.add(cls);
+    }
+
+
+    private static void parseFile(String packageName, Set<Class<?>> classSet, URL url) {
+        String packagePath = url.getPath().replaceAll("%20", " ");
+        addClass(classSet, packagePath, packageName);
+    }
+
+    private static void parseJar(Set<Class<?>> classSet, URL url) throws IOException {
+        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+        if (Objects.isNull(jarURLConnection)) {
+            return;
+        }
+        JarFile jarFile = jarURLConnection.getJarFile();
+        if (Objects.isNull(jarFile)) {
+            return;
+        }
+        Enumeration<JarEntry> jarEntries = jarFile.entries();
+        while (jarEntries.hasMoreElements()) {
+            JarEntry jarEntry = jarEntries.nextElement();
+            String jarEntryName = jarEntry.getName();
+            if (jarEntryName.endsWith(".class")) {
+                String className = jarEntryName.substring(0, jarEntryName.lastIndexOf("."))
+                        .replaceAll("/", ".");
+                doAddClass(classSet, className);
+            }
+        }
+    }
+}
